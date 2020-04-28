2cd5b217bce903af5d64375f56c62dc5f0f8fec6
konglingwen94 <794430774@qq.com>
2020-04-28 10:06:44 +0800

    初步完成食品挑选组件

---
+++ src/components/food-picker/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="food-picker">
+    <div class="reduce">
+      <i class="iconfont reduce"></i>
+    </div>
+    <div class="counter">3</div>
+    <div class="add">
+      <i class="iconfont add"></i>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+.food-picker {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  .iconfont {
+    color: blue;
+    font-size: 30px;
+  }
+  .counter {
+    margin: 0 20px;
+  }
+}
+</style>
