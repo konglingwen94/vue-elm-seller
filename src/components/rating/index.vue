3dea76ae25320d71d3fe4d50f305a55583a938f1
konglingwen94 <794430774@qq.com>
2020-05-01 23:30:50 +0800

    添加rating组件

---
+++ src/components/rating/index.vue
@@ -0,0 +1,223 @@
+<template>
+  <div class="ratings-content">
+    <div class="ratings-content-types">
+      <ul class="ratetype-options">
+        <li
+          v-for="(option,index) in rateTypeList"
+          :key="index"
+          @click.stop="toggleRateType(option.type)"
+          class="ratetype-item"
+        >
+          <span>{{option.label}}{{option.count}}</span>
+          4
+        </li>
+      </ul>
+      <div class="rate-switch">
+        <i @click.stop="toggleSwitchType" :class="{'on':switchType===1}" class="iconfont switch"></i>
+        <span class="label-text">只看有内容的评价</span>
+      </div>
+    </div>
+
+    <ul class="ratings-content-list">
+      <li v-for="(item,index) in renderList" :key="index" class="ratings-content-list-item">
+        <!-- <div> -->
+        <img
+          class="avatar"
+          width="100%"
+          height="100%"
+          src="https://img.php.cn/upload/article/000/000/006/5ea01d953886a268.jpg"
+          alt
+        />
+        <!-- </div> -->
+        <div class="information">
+          <div class="top">
+            <h3 class="nickname">nickname</h3>
+            <time>{{new Date() | dateFormat}}</time>
+          </div>
+          <div class="sub-desc">
+            <span class="score">******</span>
+            <span class="deliverytime">90分钟送达</span>
+          </div>
+          <div
+            class="content"
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium corporis voluptas voluptatibus error obcaecati iure vel possimus quisquam reprehenderit corrupti culpa quidem, quaerat ullam officiis porro! Architecto quasi in modi.</div>
+          <div class="recommendation">
+            <i class="iconfont" :class="item.rateType===0 ? 'thumb-up':'thumb-down'"></i>
+
+            <li class="recommendation-item">八宝粥</li>
+            <li class="recommendation-item">八宝粥</li>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "rating",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    rateTypeLabels: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      switchType: 0,
+      rateType: -1,
+      rateTypeOptions: [
+        { type: -1 },
+        {
+          type: 0
+        },
+        {
+          type: 1
+        }
+      ]
+    };
+  },
+  computed: {
+    rateTypeList() {
+      return this.rateTypeOptions.map((typeItem, index) => {
+        return {
+          ...item,
+          label: this.rateTypeLabels[index],
+          count: this.data.filter(item => {
+            return item.rateType === typeItem.rateType;
+          }).length
+        };
+      });
+    },
+    renderList() {
+      let renderList = [];
+      if (this.rateType === -1) {
+        renderList = this.data;
+      }
+      renderList = this.data.filter(item => item.rateType === this.rateType);
+
+      return renderList.filter(item => {
+        if (this.switchType === 1) {
+          return item.text !== "";
+        }
+        return true;
+      });
+    },
+    toggleRateType(type) {
+      this.rateType = type;
+    }
+  },
+  methods: {
+    toggleSwitchType() {
+      if (this.switchType === 0) {
+        this.switchType = 1;
+      } else {
+        this.switchType = 0;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+.ratings-content > * {
+  padding: 0 34px;
+  border: 1px solid #eee;
+  border-width: 1px 0;
+}
+
+.ratings-content-types {
+  .ratetype-options {
+    border-bottom: 1px solid #ccc;
+    display: flex;
+    padding: 38px 0;
+    .ratetype-item {
+      width: 120px;
+      line-height: 60px;
+      text-align: center;
+      margin-right: 30px;
+      border-radius: 2px;
+      background-color: rgba(0, 160, 220, 0.2);
+      &.active {
+        background-color: #00a0dc;
+        color: #fff;
+
+        &.bad {
+          background-color: #666;
+          color: #4d555d;
+        }
+      }
+    }
+  }
+  .rate-switch {
+    padding: 30px 0;
+    .switch {
+      cursor: pointer;
+      vertical-align: -7px;
+      margin-right: 4px;
+      font-size: 30px;
+      &.on {
+        color: green;
+      }
+    }
+  }
+}
+.ratings-content-list {
+  &-item {
+    display: flex;
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 35px;
+    padding-top: 35px;
+    .avatar {
+      width: 56px;
+      height: 56px;
+      border-radius: 50%;
+      margin-right: 20px;
+    }
+    .information {
+      flex: 1;
+      .top {
+        display: flex;
+        justify-content: space-between;
+        time {
+          color: #93999f;
+          font-size: 20px;
+        }
+      }
+      .sub-desc {
+        margin: 12px 0;
+        .score {
+          margin-right: 10px;
+        }
+        .deliverytime {
+          color: #93999f;
+          font-size: 20px;
+        }
+      }
+      .content {
+        margin: 14px 0;
+      }
+      .recommendation {
+        line-height: 36px;
+        display: inline-flex;
+        text-align: center;
+        align-items: center;
+        .iconfont {
+          font-size: 24px;
+          color: #00a0dc;
+        }
+        &-item {
+          font-size: 18px;
+          width: 120px;
+          color: #93999f;
+          border-radius: 2px;
+          border: solid 1px;
+          margin-left: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
