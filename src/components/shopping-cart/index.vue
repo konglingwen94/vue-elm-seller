6bed71760c5ed10619b7b2238c3040cd48cd44ce
konglingwen94 <794430774@qq.com>
2020-05-01 09:19:17 +0800

    完成购物车静态页面展示

--- src/components/shopping-cart/index.vue
+++ src/components/shopping-cart/index.vue
@@ -10,9 +10,56 @@
     <div class="minprice">
       <span class="text">20元起送</span>
     </div>
+    <div class="goods-container" v-show="visible">
+      <div class="layer"></div>
+      <div class="goods">
+        <div class="head">
+          <div class="title">购物车</div>
+          <div class="clear">清空</div>
+        </div>
+        <div class="goods-wrapper">
+          <scroll>
+            <ul class="goods-list">
+              <li v-for="(item,index) in 10" :key="index" class="goods-item">
+                <div class="name">name</div>
+                <span class="price">333</span>
+
+                <div class="action">
+                  <food-picker></food-picker>
+                </div>
+              </li>
+            </ul>
+          </scroll>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
+<script>
+import FoodPicker from "@/components/food-picker";
+import shoppingStore from "@/store/shopping-cart.js";
+export default {
+  name: "shopping-cart",
+  components: {
+    FoodPicker
+  },
+  data() {
+    return {
+      goodsList: shoppingStore.goods,
+      visible: false
+    };
+  },
+  props: {},
+  methods: {
+    show() {
+      this.visible = true;
+    },
+    hide() {
+      this.visible = false;
+    }
+  }
+};
+</script>
 <style lang="less" scoped>
 .shopping-card {
   display: flex;
@@ -29,7 +76,7 @@
   margin-left: 34px;
   .cart-icon {
     display: inline-block;
-    
+    z-index: 10030;
     border: 10px solid rgba(7, 17, 27, 1);
     border-radius: 50%;
     position: absolute;
@@ -64,5 +111,66 @@
   height: 100%;
   line-height: 94px;
   text-align: center;
+}
+.goods-wrapper {
+  height: 400px;
+  padding: 20px;
+//   position: relative;
+//   z-index: 10000;
+  background: #fff;
+}
+
+.layer {
+  z-index: 100;
+  background: #07111b;
+  opacity: 0.5;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 90px;
+}
+
+.goods-container {
+  position: absolute;
+  bottom: 90px;
+  width: 100%;
+  //   m: 0 20px;
+  z-index: 1000;
+}
+
+.goods {
+  z-index: 10000;
+  position: relative;
+  .head {
+    padding: 20px;
+
+    background-color: rgba(243, 245, 247, 1);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+    .clear {
+      color: rgb(0, 160, 220);
+    }
+  }
+  &-item {
+    display: flex;
+    padding: 20px 4px;
+    border-bottom: 1px solid #efefef;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+
+    .price {
+      margin-left: auto;
+      margin-right: 20px;
+      color: red;
+      font-size: 28px;
+    }
+    .action {
+    }
+  }
 }
 </style>
