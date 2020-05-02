67bd5de078f767f9ba23f5cf5277612bffe4e571
konglingwen94 <794430774@qq.com>
2020-05-02 10:35:08 +0800

    引入字体图标样式

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import Vue from 'vue'
  import Icon from './components/icon'
 import App from './App.vue'
 import directive from './helper/directive'
+// 引入font
+// import './assets/font.css'
 // 注册自定义指令
 Vue.use(directive)
+
+
+
 
 import 'lib-flexible'
 import 'normalize.css'
