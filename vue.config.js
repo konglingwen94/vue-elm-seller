dad7b7621931b8be137eac26f805727cde7f599a
konglingwen94 <794430774@qq.com>
2020-10-14 18:35:14 +0800

    修改API代理地址

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
- 
-
 module.exports = {
   lintOnSave: "warning",
-  // outputDir: "server/dist",
+
   publicPath: process.env.NODE_ENV !== "production" ? "/" : "./",
   devServer: {
-     
-    // proxy: "http://123.56.124.33:5000",
+    proxy: "http://123.56.124.33:5000",
   },
 };
